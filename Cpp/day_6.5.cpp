427b678fc14f097386ed134a962d2ec00cdeecde
reixyz22 <spellcasters22@gmail.com>
2026-03-14 04:45:21 -0500

    q6p1 done wow

--- Cpp/day_6.5.cpp
+++ Cpp/day_6.5.cpp
@@ -32,19 +32,52 @@ int main() {
     while (std::getline(file, line)) {
         std::istringstream iss(line);
         std::vector<char> row(line.begin(),line.end());
-
-
         homework.push_back(row);
     }
 
-    for (const auto& row : homework) {
-        for (char c : row) {
-            std::cout << c;
-        }
-    }
+    //Debugging
+    // for (const auto& row : homework) {
+    //     for (char c : row) {
+    //         std::cout << c;
+    //     }
+    // }
     // alternate style that i learned in school for syntax referance / study
     // for ( int i : homework.size())
     //     for (char c : homework[i]) {
     //         std::cout << c;
     // }
+
+    long long total = 0;
+
+    std:: vector<int> cols;
+
+    bool m = true;
+    for(int i = 0; i < homework[0].size(); i++) {
+        std::string col_string;
+        for (int j = 0; j < homework.size(); j++) {
+            if (homework[i][j] != '*') {
+                m = true;
+            }
+            else if (homework[i][j] != '+') {
+                m = false;
+            }
+            else if (homework[i][j] != ' ') {
+                col_string += homework[i][j];
+            }
+
+            if (!col_string.empty()) { // my ide told me to do this instead of col_string != "" idk why yet tbh.
+                cols.push_back(std::stoi(col_string));
+            }
+            else { // all blank col means that we hit the end of this homework question and now it's time to calculate
+                if (m) {
+                    total += multiply(cols);
+                }
+                else {
+                    total += addition(cols);
+                }
+                cols = {};
+            }
+        }
+    }
+    std::cout << total;
 }
